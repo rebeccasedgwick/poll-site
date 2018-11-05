ea642abf745a5663ae790140bfcf5ff62f914fc2
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-05 12:45:16 +0000

    fixes tests for generic responses

--- polls/tests.py
+++ polls/tests.py
@@ -37,7 +37,7 @@ class QuestionIndexViewTests(TestCase):
     def test_no_question(self):
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are currently available.")
+        self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_past_question(self):
@@ -51,7 +51,7 @@ class QuestionIndexViewTests(TestCase):
     def test_future_question(self):
         create_question(question_text="Future question", days=30)
         response = self.client.get(reverse('polls:index'))
-        self.assertContains(response, "No polls are currently available")
+        self.assertContains(response, "No polls are available")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_future_question_and_past_question(self):
